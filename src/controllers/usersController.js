048677b1bbc5f7c5aef6ebce1d20bc682375b170
Violeta Aguirre <violeta.aguirre@riseinteractive.com>
2020-12-21 10:50:50 -0300

    Proceso de login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,10 @@
-const { writeUsers } = require('../helpers/helpers');
 const helper = require('../helpers/helpers');
-const bcrypt = require('bcrypt');
+
+/* VIOLE, LE AGREGUE EL JS */
+const bcryptjs = require('bcryptjs');
+
+/* VIOLE */
+const { check, validationResult, body } = require ('express-validator')
 
 const usersController = {
 	login: (req, res) => {
@@ -10,7 +14,7 @@ const usersController = {
         res.render('register');
     }, 
     carga: (req, res) => {
-        { 
+    
             const newUser = {
                 id: helper.generateNewIdUsers(),
                 name: req.body.name,
@@ -18,7 +22,7 @@ const usersController = {
                 userName: req.body.userName,
                 birthday: req.body.birthday,
                 email: req.body.email,
-                password: bcrypt.hashSync(req.body.password, 10),
+                password: bcryptjs.hashSync(req.body.password, 10),
                 image: req.files[0].filename,
                 category: "user"
             }
@@ -28,8 +32,64 @@ const usersController = {
          helper.writeUsers(saveUser);
     
          res.redirect('/');
+ 
+    },
+
+    /* VIOLE */
+
+    processLogin: (req, res) => {
+
+        console.log(req.body)
+        const email = req.body.email;
+		const password = req.body.password;
+		const users = helper.getAllUsers();
+		const userExist = users.find((user) => {
+			return user.email == email
+		});
+
+		if (userExist && bcryptjs.compareSync(password, userExist.password)) {
+			req.session.email = email;			
+			res.redirect('/users');
+		}else{
+			res.render('login',{
+				loginError: true
+			})
+		}
+
+	}
+
+    /* processLogin: (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+        const users = helper.getAllUsers();
+
+        const userExist = users.find((user) => {
+        return user.email === email;
+        });
+
+        if (userExist && bycryptjs.compareSync(password, userExist.password)){
+        req.session.user = userExist.email;
+        console.log(userExist)
+        
+
         }
-    }
+*/
+
+        /*processLogin: (req, res) => {
+        const results = validationResult(req);
+        if (!results.isEmpty()) {
+            return res.render('login', {
+                errors: results.errors,
+                old: req.body
+            });    
+        }
+        const userFound = readJson().find(user => user.email == req.body.email);
+        req.session.user = userFound;
+        if (req.body.remember){
+            res.cookie('user', userFound.id, { maxAge: 1000 * 60 * 60 });
+        }
+        return res.redirect('/');
+    }*/
 
 }
 
