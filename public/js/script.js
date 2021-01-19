f82389d4e099a811716175552c62a48250ce49cc
guchito <a_gaggero@hotmail.com>
2021-01-19 16:00:57 -0300

    Empece con nodemailer

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,19 @@
 const selectCategory = document.querySelectorAll('.categoryFilter')
-const db = require('../../src/database/models');
+//const db = require('../../src/database/models');
 
 
 for(const category of selectCategory){
-    category.addEventListener('click',async () => {
-        const listado = document.querySelector('.articuloListado')
-        const products = await db.Product.findAll({
-			include: ["categories", "brand"],
-        });
-        
-        if(category == 0){ //0 es mostrar todos
-
-        }else{
-            products.filter(product=>product.categories[0].id == category.value)
-
+    category.addEventListener('click', async () => {
+        //const listado = document.querySelector('.articuloListado')
+        //const products = await db.Product.findAll({
+		//	include: ["categories", "brand"],
+        //});
+        const filtered = document.querySelector('#filtered')
+        if(category != 0){ //0 es mostrar todos
+            const filter = "<% products.filter(product=>product.categories[0].id == category.value) %>"
+            filtered.innerHTML = filter
         }
         
-        console.log(category.value)
         
     })
 
