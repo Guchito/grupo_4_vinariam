98d382c4b7ad4adbdc0da5873a0d83644f7be4c5
mussisofia <mussisofia@gmail.com>
2021-01-19 21:36:35 -0300

    Je

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "vinariam",
     "host": "127.0.0.1",
     "dialect": "mysql",
@@ -11,14 +11,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
