8327f36db6f54fd38d5c49aca69ab0c8648ffa68
mussisofia <mussisofia@gmail.com>
2021-01-20 21:18:42 -0300

    je

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": 'mysql',
+    "dialect": process.env.DB_DIALECT,
     "define": {
       "underscored": true
     }
