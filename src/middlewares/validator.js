6cac83f8ea24c86f0ff007c3d410f1e9957c8572
mussisofia <mussisofia@gmail.com>
2021-01-21 00:27:59 -0300

    terminos y condiciones validados

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -32,6 +32,10 @@ module.exports = {
         .custom((value, { req }) => req.files[0])
         .withMessage('La imagen de perfil es obligatoria')
         .bail(),
+        body('terminos').custom(value => {
+            return value;
+
+        }).withMessage('Debes aceptar los términos y condiciones del sitio').bail(),
         
         body('password').notEmpty().withMessage('El campo contraseña no puede estar vacío').bail()
         .isLength({min: 6, max:99}).withMessage('La contraseña debe tener como mínimo 6 caracteres').bail()
