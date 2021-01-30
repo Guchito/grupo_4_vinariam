40faf0b1e793437dd7ec0bf431b80da70ac203d2
mussisofia <mussisofia@gmail.com>
2021-01-30 17:23:59 -0300

    añadidos validaciones api

--- src/routes/apis/apiUsers.js
+++ src/routes/apis/apiUsers.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const apiUsersController = require('../../controllers/apis/apiUsersController');
 
 router.get('/', apiUsersController.list);
-//router.post('/create', apiUsersController.create);
 router.post('/login', apiUsersController.login);
 router.post('/checkEmail', apiUsersController.checkEmail);
-router.get('/checkEmail', apiUsersController.checkEmail);
 
 module.exports = router;
