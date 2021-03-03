fce851df69fd16a1709c9b66ce674951f7ba6c2f
mussisofia <mussisofia@gmail.com>
2021-03-02 21:20:14 -0300

    hola

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+  "production": {    
+    "username": process.env.DB_USERNAME,    
+    "password": process.env.DB_PASSWORD,   
+    "database": process.env.DB_NAME,    
+    "host": process.env.DB_HOST,    
     "dialect": "mysql"  
   }
 }
