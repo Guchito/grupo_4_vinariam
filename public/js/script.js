36c57ad1916d2782bcb4db868a0349188cbd58fe
Agustin <a_gaggero@hotmail.com>
2021-03-05 21:39:37 -0300

    Arregle el filter de vinos que solo andaba en firefox...

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,10 @@
-const selectCategory = document.querySelectorAll('.categoryFilter')
-for(const category of selectCategory){
-    category.addEventListener('click', () => {
-        const selected = document.querySelectorAll(`[data-category="${category.value}"]`)
+const selectCategory = document.querySelectorAll('.categoryFilter');
+const selectFilter = document.querySelector('.selectFilter');
+
+selectFilter.addEventListener('change',(e)=>{     
+    for(const category of selectCategory){
+    
+        const selected = document.querySelectorAll(`[data-category="${e.target.value}"]`)
         const all = document.querySelectorAll('.articuloListado')
         for (const one of all){
             one.style.display='none'
@@ -14,5 +17,6 @@ for(const category of selectCategory){
                 one.style.display='block'
             }
         }
-    })
-}
+        
+    }
+})
