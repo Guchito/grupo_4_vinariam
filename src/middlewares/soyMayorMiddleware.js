9ca627d99af58176daf79342d39680ab644283ee
guchito <a_gaggero@hotmail.com>
2024-12-27 13:45:38 +0100

    Translate text to english

--- src/middlewares/soyMayorMiddleware.js
+++ src/middlewares/soyMayorMiddleware.js
@@ -2,9 +2,9 @@ const soyMayorMiddleware = function(req,res,next) {
     if(req.originalUrl == '/enter'){
         next();
     }else{
-        // const originalUrl = req.originalUrl;
-        // module.exports = originalUrl;
-        // console.log('En el middleware: ' + originalUrl);
+        const originalUrl = req.originalUrl;
+        module.exports = originalUrl;
+        console.log('En el middleware: ' + originalUrl);
         if (req.session.mayor){
             next();
         }
