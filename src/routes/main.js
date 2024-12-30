23a22a695ce31dcfb45f572e36e9ed27d28e6897
guchito <a_gaggero@hotmail.com>
2024-12-30 14:40:09 +0100

    Checking if the variables are ok with vercel

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,6 +5,13 @@ const mainController = require('../controllers/mainController'); //requiero al c
 
 /* ROUTES MAIN */
 
+router.get('/check-env', (req, res) => {
+    res.json({
+        DB_HOST: process.env.DB_HOST,
+        DB_USER: process.env.DB_USER,
+        DB_NAME: process.env.DB_NAME,
+    });
+});
 router.get('/', mainController.index);
 router.get('/enter', mainController.enter);
 router.post('/enter', mainController.soyMayor);
